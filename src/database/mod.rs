e334d5a433c44b2967e0233cc8e3804addfee6a1
JohnTheCoolingFan <ivan8215145640@gmail.com>
2023-09-30 00:18:04 +0300

    Added functions to create optional tables

--- src/database/mod.rs
+++ src/database/mod.rs
@@ -9,3 +9,23 @@ pub async fn connect_db() -> Result<Pool<MySql>, sqlx::Error> {
         )
         .await
 }
+
+pub async fn create_ip_recording_tables(db: &Pool<MySql>) -> Result<(), sqlx::Error> {
+    log::debug!("Creating `origins` table");
+    sqlx::query_file!("db/optional/origins.sql")
+        .execute(db)
+        .await?;
+    log::debug!("Creating `strikes` table");
+    sqlx::query_file!("db/optional/strikes.sql")
+        .execute(db)
+        .await?;
+    Ok(())
+}
+
+pub async fn create_token_tables(db: &Pool<MySql>) -> Result<(), sqlx::Error> {
+    log::debug!("Creating `tokens` table");
+    sqlx::query_file!("db/optional/tokens.sql")
+        .execute(db)
+        .await?;
+    Ok(())
+}
