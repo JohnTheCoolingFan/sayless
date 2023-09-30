d5778776346cb9d7ad95e8a18323fa22fe51f138
JohnTheCoolingFan <ivan8215145640@gmail.com>
2023-09-30 03:50:10 +0300

    Added status endpoint

--- src/routes/mod.rs
+++ src/routes/mod.rs
@@ -1,7 +1,7 @@
 use crate::{
     routes::{
         create_link::create_link_route, create_token::create_token_route, get_link::get_link_route,
-        get_link_info::get_link_info_route,
+        get_link_info::get_link_info_route, status::status_route,
     },
     service_config::ServiceConfig,
     ServiceState,
@@ -15,11 +15,13 @@ pub mod create_link;
 pub mod create_token;
 pub mod get_link;
 pub mod get_link_info;
+pub mod status;
 
 pub fn create_router(config: &ServiceConfig) -> Router<ServiceState> {
     log::info!("Building router");
     let mut router = Router::new()
         .route("/l/create", post(create_link_route))
+        .route("/l/status", get(status_route))
         .route("/l/:id", get(get_link_route))
         .route("/l/:id/info", get(get_link_info_route));
 
