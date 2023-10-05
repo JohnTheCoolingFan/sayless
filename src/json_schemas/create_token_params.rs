94574175a902842f493a13ed652a93e9eb9fbb50
JohnTheCoolingFan <ivan8215145640@gmail.com>
2023-10-05 13:33:24 +0300

    Add optional `expires_at` property for token creation

--- src/json_schemas/create_token_params.rs
+++ src/json_schemas/create_token_params.rs
@@ -1,4 +1,5 @@
-use serde::Deserialize;
+use chrono::{DateTime, NaiveDateTime, Utc};
+use serde::{Deserialize, Deserializer};
 
 #[derive(Deserialize)]
 pub struct CreateTokenParams {
@@ -10,4 +11,14 @@ pub struct CreateTokenParams {
     pub create_token_perm: bool,
     #[serde(default)]
     pub view_ips_perm: bool,
+    #[serde(default, deserialize_with = "deser_timestamp")]
+    pub expires_at: Option<DateTime<Utc>>,
+}
+
+fn deser_timestamp<'de, D: Deserializer<'de>>(des: D) -> Result<Option<DateTime<Utc>>, D::Error> {
+    Ok(Some(
+        NaiveDateTime::parse_from_str(&String::deserialize(des)?, "%Y-%m-%d %H:%M:%S%.f")
+            .map_err(serde::de::Error::custom)?
+            .and_utc(),
+    ))
 }
