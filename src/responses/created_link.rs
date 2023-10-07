b998754707ea5f8a7ea4643bc3e3d65664b38d59
JohnTheCoolingFan <ivan8215145640@gmail.com>
2023-10-07 17:38:52 +0300

    Use axum::http:header instead of custom const

--- src/responses/created_link.rs
+++ src/responses/created_link.rs
@@ -2,9 +2,6 @@ use axum::{
     http::StatusCode,
     response::{IntoResponse, Response},
 };
-use headers::HeaderName;
-
-const LOCATION_HEADER: HeaderName = HeaderName::from_static("location");
 
 #[derive(Debug, Clone)]
 pub struct CreatedLink {
@@ -15,7 +12,7 @@ impl IntoResponse for CreatedLink {
     fn into_response(self) -> Response {
         Response::builder()
             .status(StatusCode::CREATED)
-            .header(LOCATION_HEADER, format!("/l/{}", self.id))
+            .header(axum::http::header::LOCATION, format!("/l/{}", self.id))
             .body(Default::default())
             .unwrap()
     }
