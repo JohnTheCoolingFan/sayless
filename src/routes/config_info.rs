85e96ffc845da37c2eeec138ef8763fc259648a3
JohnTheCoolingFan <ivan8215145640@gmail.com>
2023-12-17 16:17:03 +0300

    Replace status endpoint with config info

---
+++ src/routes/config_info.rs
@@ -0,0 +1,129 @@
+use std::sync::Arc;
+
+use axum::{
+    extract::State,
+    http::StatusCode,
+    response::{IntoResponse, Response},
+    Json, TypedHeader,
+};
+use chrono::Duration;
+use serde::{Serialize, Serializer};
+
+use crate::{custom_headers::accept::Accept, service_config::ServiceConfig, ServiceState};
+
+#[derive(Debug, Clone)]
+pub enum ConfigInfoResponse {
+    Json(Json<ConfigInfo>),
+    String(String),
+}
+
+impl IntoResponse for ConfigInfoResponse {
+    fn into_response(self) -> Response {
+        match self {
+            Self::Json(json) => json.into_response(),
+            Self::String(string) => string.into_response(),
+        }
+    }
+}
+
+#[derive(Debug, Clone, Serialize)]
+pub struct ConfigInfo {
+    pub service_version: &'static str,
+    pub max_strikes: u16,
+    pub log_level: log::Level,
+    pub ip_recording: Option<IpRecordingConfigInfo>,
+    pub tokens: Option<TokenConfigInfo>,
+}
+
+#[derive(Debug, Clone, Serialize)]
+pub struct IpRecordingConfigInfo {
+    #[serde(serialize_with = "duration_to_str_serialize")]
+    pub retention_period: Duration,
+    pub retention_check_period: Arc<str>,
+}
+
+fn duration_to_str_serialize<S: Serializer>(dur: &Duration, ser: S) -> Result<S::Ok, S::Error> {
+    dur.to_string().serialize(ser)
+}
+
+#[derive(Debug, Clone, Serialize)]
+pub struct TokenConfigInfo {
+    link_creation_requires_auth: bool,
+}
+
+pub async fn config_info_route(
+    State(ServiceState { db: _, config }): State<ServiceState>,
+    accept: Option<TypedHeader<Accept>>,
+) -> Result<ConfigInfoResponse, (StatusCode, String)> {
+    let accept = accept.unwrap_or(TypedHeader(Accept(mime::APPLICATION_JSON)));
+
+    if accept.0 .0 == mime::APPLICATION_JSON {
+        Ok(ConfigInfoResponse::Json(config_info_json_handler(config)))
+    } else if accept.0 .0 == mime::TEXT_PLAIN {
+        Ok(ConfigInfoResponse::String(config_info_text_handler(config)))
+    } else {
+        Err((
+            StatusCode::BAD_REQUEST,
+            "Unsupported format requested".to_string(),
+        ))
+    }
+}
+
+pub fn config_info_json_handler(config: ServiceConfig) -> Json<ConfigInfo> {
+    Json(ConfigInfo {
+        service_version: env!("CARGO_PKG_VERSION"),
+        max_strikes: config.max_strikes,
+        log_level: log::max_level()
+            .to_level()
+            .expect("Logging shouldn't be turned off"),
+        ip_recording: config.ip_recording.map(|iprc| IpRecordingConfigInfo {
+            retention_period: iprc.retention_period,
+            retention_check_period: iprc.retention_check_period,
+        }),
+        tokens: config.token_config.map(|tkc| TokenConfigInfo {
+            link_creation_requires_auth: tkc.creation_requires_auth,
+        }),
+    })
+}
+
+pub fn config_info_text_handler(config: ServiceConfig) -> String {
+    let ServiceConfig {
+        max_strikes,
+        ip_recording,
+        token_config: tokens,
+        log_level: _,
+    } = config;
+    let ip_recording_status = if ip_recording.is_some() {
+        "Enabled"
+    } else {
+        "Disabled"
+    };
+    let tokens_status = if tokens.is_some() {
+        "Enabled"
+    } else {
+        "Disabled"
+    };
+    let creation_requires_auth = if let Some(toks) = tokens {
+        toks.creation_requires_auth
+    } else {
+        false
+    };
+    format!(
+        r#"
+        Sayless v{} configuration info
+
+        IP recording: {};
+        Max amount of strikes: {};
+        Token authentication: {};
+        Link creation requires authentication: {};
+
+        Log level: {}
+        "#,
+        env!("CARGO_PKG_VERSION"),
+        ip_recording_status,
+        max_strikes,
+        tokens_status,
+        creation_requires_auth,
+        log::max_level(),
+    )
+}
--- src/routes/mod.rs
+++ src/routes/mod.rs
@@ -1,30 +1,32 @@
-use crate::{
-    routes::{
-        create_link::create_link_route, create_token::create_token_route, get_link::get_link_route,
-        get_link_info::get_link_info_route, revoke_token::revoke_token_route, status::status_route,
-    },
-    service_config::ServiceConfig,
-    ServiceState,
-};
 use axum::{
     routing::{get, post},
     Router,
 };
 
+use crate::{
+    routes::{
+        config_info::config_info_route, create_link::create_link_route,
+        create_token::create_token_route, get_link::get_link_route,
+        get_link_info::get_link_info_route, revoke_token::revoke_token_route,
+    },
+    service_config::ServiceConfig,
+    ServiceState,
+};
+
+pub mod config_info;
 pub mod create_link;
 pub mod create_token;
 pub mod get_link;
 pub mod get_link_info;
 pub mod revoke_token;
-pub mod status;
 
 pub fn create_router(config: &ServiceConfig) -> Router<ServiceState> {
     log::info!("Building router");
     let mut router = Router::new()
         .route("/l/create", post(create_link_route))
-        .route("/l/status", get(status_route))
         .route("/l/:id", get(get_link_route))
-        .route("/l/:id/info", get(get_link_info_route));
+        .route("/l/:id/info", get(get_link_info_route))
+        .route("/l/config_info", get(config_info_route));
 
     if config.token_config.is_some() {
         router = router
